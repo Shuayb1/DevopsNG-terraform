5abd4700f24d82505c88b140813bf7513203803f
Shuayb Popoola <shuaybpopoola@gmail.com>
2021-09-21 12:54:30 +0100

    manifest to deploy 1 instance

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "devopsng" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
